0e9ae1e09e4fa56703624d2ee6b34c35be6d23ad
twothicc <61146198+twothicc@users.noreply.github.com>
2020-10-04 22:03:53 +0800

    more problems

---
+++ more_algorithms.py
@@ -0,0 +1,13 @@
+
+#Find the binary, octal and hexadecimal expansions of 1801131029198.
+#binary shld give [1, 1, 0, 1, 0, 0, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 1, 0]
+#octal shld give  [3, 2, 1, 5, 3, 3, 6, 1, 4, 7, 1, 3, 1, 6]
+#hexadecimal shld give [1, A, 3, 5, B, C, 6, 7, 2, C, E]
+
+#Implement #quicksort with runtime complexity of O(n * log n)
+
+#Implement radix_sort() with runtime complexity of O((n+b) * logb(k)) where b is the base and k is the largest possible value
+
+#Finds combinations of values in list that would give a target value
+#print(combinations([1,5,5,10,20,5,10], 0, 25))
+#shld give [[5, 5, 10, 5], [5, 5, 5, 10], [5, 10, 10], [5, 20], [5, 10, 10], [5, 20], [10, 5, 10], [20, 5]]
